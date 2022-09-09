da7d38535f225380b1744c6d06b71d59558cde72
OLEH VIESHAIEV <sred_a@i.ua>
2022-09-09 12:25:05 +0300

    fixed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,28 +1,27 @@
 import java.util.List;
-import java.util.Random;
 import model.Building;
 import model.Floor;
 import service.BuildingService;
 import service.ElevatorService;
 import service.FloorService;
+import service.Generator;
 import service.impl.BuildingServiceImpl;
 import service.impl.ElevatorServiceImpl;
 import service.impl.FloorServiceImpl;
+import service.impl.GeneratorImpl;
 
 public class Application {
     private static final Integer MIN_FLOOR = 5;
     private static final Integer MAX_FLOOR = 20;
-
     public static void main(String[] args) {
-
-        Random random = new Random();
-        Integer floorQuantity = random.ints(MIN_FLOOR, MAX_FLOOR + 1).findFirst().getAsInt();
-        BuildingService buildingService = new BuildingServiceImpl();
+        Generator generator = new GeneratorImpl();
+        Integer floorQuantity = generator.generateFloorQuantity(MIN_FLOOR, MAX_FLOOR);
+        BuildingService buildingService = new BuildingServiceImpl(generator);
         Building building = buildingService.create(floorQuantity);
-        FloorService floorService = new FloorServiceImpl();
+        FloorService floorService = new FloorServiceImpl(generator);
         floorService.generatePassengersOnFloor(building.getFloors());
         List<Floor> floors = building.getFloors();
-        ElevatorService elevatorService = new ElevatorServiceImpl();
+        ElevatorService elevatorService = new ElevatorServiceImpl(generator);
         elevatorService.run(floors);
 
     }
